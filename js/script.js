aefbde0097b862ceca3cce0d0ba59582d2cdcaf4
akhielhidajatgabrut <akhielhidajat0.com>
2023-07-06 15:49:01 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const navbarNav = document.querySelector(".navbar-nav");
+const hamburger = document.querySelector("#hamburger-menu");
+
+hamburger.onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+document.addEventListener("click", (e) => {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
